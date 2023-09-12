bdc2b04c52f8965ce629156769e341bf3a23edad
Nazmul Hussain <nazmulhussain.api@gmail.com>
2023-09-12 13:38:33 +0600

    Add global env

---
+++ public/config.js
@@ -0,0 +1,3 @@
+window.global_ENV = {
+	API_URL: 'http://13.213.162.196/HAEFA_API',
+};
--- src/helper/Constants.js
+++ src/helper/Constants.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://192.168.10.40/haefa-all"
 // export const API_URL = "http://192.168.10.30:4444"
-export const API_URL = "http://13.213.162.196/HAEFA_API"
+export const API_URL = window.global_ENV.API_URL
