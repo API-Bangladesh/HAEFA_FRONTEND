45a8db860b589ad5537846711d9c85491f50b31b
Nazmul Hussain <nazmulhussain.api@gmail.com>
2023-09-14 12:06:11 +0600

    Remove cdn dependency

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/css/animate.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./assets/css/style.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store/store";
